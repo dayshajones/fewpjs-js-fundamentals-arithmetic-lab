dfd88603fe9c478dde7f7451a59d9bce836aabf7
“Daysha <+>
2022-01-31 16:04:29 -0800

    commit tree be8e8c0a81d9b4f5ae82dedd67ce0bb563738e96

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+let multiply = num1 * num2;
+multiply;
+
+let random = Math.ceil(Math.random());
+random;
+
+let num3 = 24;
+let num4 = 5;
+let mod = num3 % num4;
+mod;
+
+let max = Math.max(1, 2, 20, 6);
+max;
+
